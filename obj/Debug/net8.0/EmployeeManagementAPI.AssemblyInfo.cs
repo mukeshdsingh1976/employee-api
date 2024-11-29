64b1787c40c253052dcff2d3024579ed4c5cbcb0
shivmuk@gmail.com <shivmuk@gmail.com>
2024-11-30 00:46:44 +0530

    sonarqube

--- obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e123f98568ee9455000c5058a14c3414438266cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82341ad511f0895b1d5bd92196a95913c927da8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
