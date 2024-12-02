9aa99fa058fb9508247b76e2f21cb7f02978222f
shivmuk@gmail.com <shivmuk@gmail.com>
2024-12-02 23:13:58 +0530

    Fixed code issue

--- employee-api/Controllers/EmployeeController.cs
+++ employee-api/Controllers/EmployeeController.cs
@@ -30,7 +30,6 @@ namespace EmployeeManagementAPI.Controllers
         public IActionResult Create(Employee employee)
         {
             _service.Add(employee);
-            console.saurav("error");
             return CreatedAtAction(nameof(GetById), new { id = employee.Id }, employee);
         }
 
